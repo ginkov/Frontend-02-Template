feb8f538c10f8068af587607619ede2907c45d17
yinxin <yinxin@cmgos.com>
2020-07-23 18:59:05 +0800

    thursday

--- week04/parser.js
+++ week04/parser.js
@@ -1,4 +1,8 @@
-const { stat } = require("fs")
+// const { stat } = require("fs")
+let  currentToken = null
+function emit(token) {
+    console.log(token)
+}
 const EOF= Symbol("EOF")  // End Of File
 // 利用 Symbol 的唯一性，创建了 EOF
 
@@ -7,10 +11,16 @@ function data(c) {
         return tagOpen
     }
     else if(c == EOF) {
-        return
+        emit({
+            type: 'EOF'
+        })
     }
     else {
         // 除了 < 之外的所有字符都被理解为文本节点
+        emit({
+            type: 'text',
+            content: c
+        })
         return data
     }
 }
@@ -22,6 +32,10 @@ function tagOpen(c) {
     }
     else if(c.match(/^[a-zA-Z]$/)) {
         // 如果是英文字母，这要么是一个开始标签，要么是一个自封闭标签
+        currentToken = {
+            type: 'startTag',
+            tagName: ''
+        }
         return tagName(c)
     }
     else {
@@ -37,9 +51,11 @@ function tagName(c) {
         return selfClosingStartTag
     }
     else if(c.match(/^[a-zA-Z]$/)) {
+        currentToken.tagName += c//.toLowerCase()
         return tagName
     }
     else if(c === '>') {
+        emit(currentToken)
         return data
     }
     else {
@@ -78,9 +94,10 @@ function selfClosingStartTag(c) {
 function endTagOpen(c) {
     // 结束标签的开始
     if(c.match(/^[a-zA-Z]$/)) {
-        // currentToken = {
-        //     type: 'endTag'
-        // }
+        currentToken = {
+            type: 'endTag',
+            tagName: ''
+        }
         return tagName(c)
     }
     else if(c === '>') {
@@ -95,6 +112,7 @@ function endTagOpen(c) {
 }
 module.exports.parseHTML = function parseHTML(html) {
     // console.log(html) // 简单的占位方法
+    console.log('hahahahahhahahahahhahahahaha')
     let state = data
     for(let c of html) {
         state = state(c)
