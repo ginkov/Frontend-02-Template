66dcdfb596bb8f08014282d03ca8ba90ad09bff1
yinxin <yinxin@cmgos.com>
2020-10-11 22:17:03 +0800

    week14 first

--- week14/NOTE.md
+++ week14/NOTE.md
@@ -1,1 +1,78 @@
-学习笔记
+学习笔记 
+
+Week14 组件化
+
+# 1 手势与动画 | 手势的基本知
+
+gesture 
+
+之前轮播图的问题：
+
+1. 有些不稳定，总是容易触发
+
+怎么区分点击和拖拽的行为。
+
+把鼠标的 down move up 统一到 start move end 模型里面。
+
+start --(end) --> tap
+
+start --(移动10px) --> pan start  --(move) --> pan --(end) --> pan end
+
+pan --(move) --> pan
+
+pan --(end且有一定速度) --> flick (清扫，也就 swipe)
+
+// 10px 是容错范围，在 Retina 屏上是10px, 一倍屏是 5px; 三倍屏是 15px;
+
+// 有一些框架，比如 swipejs 就是处理手势和轮播的。
+
+start --(0.5s) --> press start （按压，而不是 tap)
+
+press start --(移动10px) --> pan start
+
+press start --(end) --> press end (停留的一段时间再松开)
+
+这里没有做双指手势。
+
+# 2 | 实现鼠标操作
+
+gesture.js
+
+gesture.html
+
+移动端没有 Mouse，就是 touch 系列的操作。
+
+touch 系列一旦 start 就一定会触发 Move 操作。
+
+touch 事件比鼠标事件多了 touchcancel
+
+### touchend vs touchcancel
+
+cancel 是以异常的模式结束的。
+
+比如，你正在touch-move，结果页面突然弹窗，就产生了 cancel 事件。
+
+系统事件可能打断 touch move 就产生了 cancel 事件。
+
+### 统一用处理手势和鼠标事件
+
+start, move, end
+
+# 3 | 实现手势的逻辑
+
+![image-20201011160716687](C:\Users\dell\AppData\Roaming\Typora\typora-user-images\image-20201011160716687.png)
+
+在 start 时进行三个判断：
+
+1. 是否 end
+2. 是否移动 10px
+3. 是否停了 0.5s 
+
+# 4 | 处理鼠标事件
+
+# 5 | 派发事件
+
+# 6 | 实现一个 flick 事件
+
+# 7 | 封装
+
---
+++ week14/gesture.js
@@ -0,0 +1,129 @@
+let element = document.documentElement;
+
+element.addEventListener('mousedown', event => {
+    let context = Object.create(null) // 好习惯
+    // mousemove 事件有一个 button 属性，是5位二进制，按掩码取的
+    // 我们要把 mousedown 的这个按键，与 mousemove 的相对应
+    contexts.set("mouse"+ (1 << event.button), context);
+    //统一用 start, move, end 处理鼠标事件
+    start(event, context);
+    let mousemove = event => {
+        let button = 1;
+        // event.buttons 用二进制掩码表示鼠标哪个键被按下来了
+        while(button <= event.buttons) {
+            let context = contexts.get("mouse" + event.button)
+            move(event, context)
+            button = button << 1;
+        }
+
+-    };
+    let mouseup = event => {
+        end(event, context)
+        element.removeEventListener('mousemove', mousemove);
+        element.removeEventListener('mouseup', mouseup);
+    };
+    // 其实里面可以三个参数, capture, once, passive
+    // capture -- capture 模式
+    // once -- 执行一次清除掉
+    // passive -- preventDefault
+    element.addEventListener('mousemove', mousemove);
+    element.addEventListener('mouseup', mouseup);
+})
+
+let contexts = new Map();
+
+// touch 系列的就这样去监听，因为你不可能越过 touch start 去 touch move
+// 不像鼠标一直可以 move
+// touch 事件里面有多个触点
+element.addEventListener("touchstart", event=>{
+    for(let touch of event.changedTouches) {
+        let context = Object.create(null);
+        contexts.set(touch.identifier, context)
+        start(touch, context);
+    }
+})
+
+// 在 Move 的时候，要有 Id 去追踪，到底是哪个点在 Move
+element.addEventListener("touchmove", event=>{
+    for(let touch of event.changedTouches) {
+        let context = contexts.get(touch.identifier)
+        move(touch, context)
+    }
+})
+
+element.addEventListener("touchend", event=>{
+    for(let touch of event.changedTouches) {
+        let context = contexts.get(touch.identifier)
+        end(touch, context)
+        contexts.delete(touch.identifier)
+    }
+})
+
+element.addEventListener("touchcancel", event=>{
+    for(let touch of event.changedTouches) {
+        let context = contexts.get(touch.identifier)
+        cancel(touch, context)
+        contexts.delete(touch.identifier)
+    }
+})
+
+// 触屏有多个 touch, 鼠标有左右键, 因此下面这些不适合作全局变量
+// 在函数调用时搞一个 context
+let handler;
+let startX, startY;
+let isPan = false, isTap = true, isPress = false;
+
+let start = (point, context) => {
+    // console.log("start", point.clientX, point.clientY)
+    context.startX = point.clientX, context.startY = point.clientY;
+
+    context.isTap = true;
+    context.isPan = false;
+    context.isPress = false;
+
+    context.handler = setTimeout(()=>{
+        context.isTap = false;
+        context.isPan = false;
+        context.isPress = true;
+        context.handler = null; // 避免多次 clear
+        console.log("press")
+    }, 500)
+}
+
+let move =(point, context) => {
+    // 是否移动 10px
+    let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
+
+    // 距离大于10
+    if(!context.isPan && dx ** 2 + dy **2 > 100) {
+        context.isTap = false;
+        context.isPan = true;
+        context.isPress = false;
+        console.log("panstart");
+        clearTimeout(context.handler)
+    }
+
+    if(context.isPan) {
+        console.log('dx, dy= ', dx, dy)
+        console.log("pan")
+    }
+}
+
+let end = (point, context) => {
+    if(context.isTap) {
+        console.log("tap")
+        clearTimeout(context.handler)
+    }
+    if(context.isPan) {
+        console.log("panend")
+    }
+    if(context.isPress) {
+        console.log("pressend")
+    }
+    // console.log("end", point.clientX, point.clientY)
+}
+
+let cancel = (point, context) => {
+    clearTimeout(context.handler)
+    console.log("cancel", point.clientX, point.clientY)
+}
